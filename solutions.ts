c6ec373bb6c33b76db847703f39d7f7fcca9b75b
geekrover <maherulhassan1@gmail.com>
2026-05-07 00:21:37 +0600

    first problem solved

---
+++ solutions.ts
@@ -0,0 +1,9 @@
+// problem 1:
+const filterEvenNumbers = (arr: number[]) => {
+  const newArr: number[] = arr.filter((num) => num % 2 === 0);
+  console.log(newArr);
+};
+
+filterEvenNumbers([1, 2, 3, 4, 5, 6]);
+
+// problem 2:
