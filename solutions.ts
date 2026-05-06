fa4e5b12235cb95e203e30a0a24be49c4a9e20b1
geekrover <maherulhassan1@gmail.com>
2026-05-07 00:52:47 +0600

    problem 3 is solved

--- solutions.ts
+++ solutions.ts
@@ -7,7 +7,6 @@ const filterEvenNumbers = (arr: number[]): number[] => {
 filterEvenNumbers([1, 2, 3, 4, 5, 6]);
 
 // problem 2:
-
 const reverseString = (inp: string): string => {
   let newStr: string = "";
   for (let i = inp.length - 1; i >= 0; i--) {
@@ -18,3 +17,22 @@ const reverseString = (inp: string): string => {
 };
 
 reverseString("typescript");
+
+// problem 3:
+type StringOrNumber = string | number;
+
+const checkType = (str: StringOrNumber) => {
+  if (typeof str === "string") {
+    return "String";
+  } else if (typeof str === "number") {
+    return "Number";
+  }
+};
+
+// Sample Input 1:
+checkType("Hello");
+
+// Sample Input 2:
+checkType(42);
+
+
